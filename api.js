cfc87fee6dccb2ca2b75db14c73d2ba8a49ab94b
narkeeso@gmail.com <narkeeso@gmail.com>
2014-06-30 00:52:49 +0000

    Initial docset push

---
+++ api.js
@@ -0,0 +1,20 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Validator.Result",
+        "Validator.Results",
+        "Validator.Rule",
+        "Validator.Rules",
+        "Validator.Support"
+    ],
+    "modules": [
+        "Ember.Validator"
+    ],
+    "allModules": [
+        {
+            "displayName": "Ember.Validator",
+            "name": "Ember.Validator"
+        }
+    ]
+} };
+});
